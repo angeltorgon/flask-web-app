4257a3d8f60a9d254475a21e4bd5d0c0fbc0b74e
angel-torres <alas4224@gmail.com>
2020-04-09 12:29:00 -0500

    updated template

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "<h1>Hello World!</h1>"
+    user = { "username": "Juanito" }
+    return render_template('index.html', title='Home', user=user)
 
