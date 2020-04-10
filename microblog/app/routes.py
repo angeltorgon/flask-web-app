67b97e9f615c84539182b9ce9a6e876b4e871218
angel-torres <alas4224@gmail.com>
2020-04-09 19:16:45 -0500

    added a base template

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,5 +5,19 @@ from app import app
 @app.route('/index')
 def index():
     user = { "username": "Juanito" }
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
 
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
